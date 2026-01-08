6d51ad44d35bdb00aeb9ff57df9183c0e5c471a2
Maxomatic458 <104733404+maxomatic458@users.noreply.github.com>
2026-01-09 00:28:51 +0100

    fix swapped chunk pos encoding

--- crates/valence_protocol/src/packets/play/forget_level_chunk_s2c.rs
+++ crates/valence_protocol/src/packets/play/forget_level_chunk_s2c.rs
@@ -1,6 +1,16 @@
 use crate::{ChunkPos, Decode, Encode, Packet};
 
-#[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
+#[derive(Copy, Clone, Debug, Decode, Packet)]
 pub struct ForgetLevelChunkS2c {
     pub pos: ChunkPos,
+}
+
+// Note: The order is inverted, because the client reads this packet as 
+// one big-endian Long, with Z being the upper 32 bits. 
+// https://minecraft.wiki/w/Java_Edition_protocol/Packets#Unload_Chunk
+impl Encode for ForgetLevelChunkS2c {
+    fn encode(&self, mut w: impl std::io::Write) -> anyhow::Result<()> {
+        self.pos.z.encode(&mut w)?;
+        self.pos.x.encode(&mut w)
+    }
 }
