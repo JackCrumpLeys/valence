b77182739abfb83bba9bd3d942650c21c98bf30e
Job Paardekooper <jobpaardekooper@gmail.com>
2026-05-27 23:14:10 +0200

    Align server_links packets with the 1.21.5 wiki

--- crates/valence_protocol/src/packets/configuration/server_links_s2c.rs
+++ crates/valence_protocol/src/packets/configuration/server_links_s2c.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::io::Write;
 
 use valence_binary::{Decode, Encode, TextComponent};
 
@@ -16,13 +17,38 @@ pub struct ServerLink<'a> {
     pub url: Cow<'a, str>,
 }
 
-#[derive(Clone, Debug, Encode, Decode)]
+#[derive(Clone, Debug)]
 pub enum ServerLinkEnum {
     BuiltIn(BuiltInLinkType),
     CustomText(TextComponent),
 }
 
-#[derive(Clone, Debug, Encode, Decode)]
+impl Encode for ServerLinkEnum {
+    fn encode(&self, mut w: impl Write) -> anyhow::Result<()> {
+        match self {
+            ServerLinkEnum::BuiltIn(label) => {
+                true.encode(&mut w)?;
+                label.encode(w)
+            }
+            ServerLinkEnum::CustomText(label) => {
+                false.encode(&mut w)?;
+                label.encode(w)
+            }
+        }
+    }
+}
+
+impl Decode<'_> for ServerLinkEnum {
+    fn decode(r: &mut &[u8]) -> anyhow::Result<Self> {
+        Ok(if bool::decode(r)? {
+            ServerLinkEnum::BuiltIn(Decode::decode(r)?)
+        } else {
+            ServerLinkEnum::CustomText(Decode::decode(r)?)
+        })
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, Encode, Decode)]
 pub enum BuiltInLinkType {
     BugReport,
     CommunityGuidelines,
