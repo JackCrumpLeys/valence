e77d9ff461a5197df8121784a3b365ed3327a49d
Job Paardekooper <jobpaardekooper@gmail.com>
2026-05-30 17:31:58 +0200

    Make sure all `window_id`s are `VarInt` instead of u8

--- crates/valence_protocol/src/packets/play/horse_screen_open_s2c.rs
+++ crates/valence_protocol/src/packets/play/horse_screen_open_s2c.rs
@@ -4,7 +4,7 @@ use crate::Packet;
 
 #[derive(Copy, Clone, Debug, Encode, Decode, Packet)]
 pub struct HorseScreenOpenS2c {
-    pub window_id: u8,
+    pub window_id: VarInt,
     pub slot_count: VarInt,
     pub entity_id: i32,
 }
